d81777d185cc4b49974849dba13b992b3d511dce
shanieyal <shani3600@gmail.com>
2016-01-31 15:21:38 +0200

    game class
    add game class for the game, must use it guy

---
+++ GameClass.py
@@ -0,0 +1,65 @@
+__author__ = 'User'
+
+
+import pygame
+
+
+class Player:  # class that deals with the player
+    def __init__(self, current_row, current_column):
+        self.x = current_row
+        self.y = current_column
+
+    def get_pose(self):
+        return self.x, self.y
+
+    def set_pose(self, x, y):
+        self.x = x
+        self.y = y
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+
+class Greed:  # game-board class
+    def __init__(self, row_block_num, column_block_num, screen, player):  # work, don't touch it
+        self.num_of_dots = 0
+        self.width = 20
+        self.height = 20
+        self.margin = 5
+        self.row_block_num = row_block_num
+        self.column_block_num = column_block_num
+        self.screen = screen
+        self.grid = []
+        row = []
+        # declaring the grid as a kind of 2d array but with list
+        for i in xrange(row_block_num):
+            row.append(3)
+        for i in xrange(column_block_num):
+            self.grid.append(row)
+        #add the player to the grid
+        self.player = player
+        self.place_player(self.player)
+
+    def draw_grid(self):  # work, don't touch it
+        for row in range(self.row_block_num):
+            row_progress = (row * self.width) + (self.margin * row) + self.margin
+            for column in range(self.column_block_num):
+                progress = (column * self.width) + (self.margin * column)
+                if self.grid[row][column] == 1:  # player spot
+                    pygame.draw.rect(self.screen, GREEN, [progress + self.margin, row_progress, self.width, self.height])
+                elif self.grid[row][column] == 3:  # point that pac-man will eat
+                    pygame.draw.rect(self.screen, YELLOW, [progress + self.margin, row_progress, self.width, self.height])
+                    self.num_of_dots += 1
+                elif self.grid[row][column] == 0:  # blank space
+                    pygame.draw.rect(self.screen, WHITE, [progress + self.margin, row_progress, self.width, self.height])
+        return self.num_of_dots
+
+    def change_pos(self, player, new_x, new_y):  # MUST BE DONE
+        pass
+
+    def place_player(self, player):  # placing the player on the grid, almost works
+        x, y = player.get_pose()
+        self.grid[x][y] = 1
--- mainGame.py
+++ mainGame.py
@@ -1,68 +1,13 @@
 __author__ = 'guypc'
 
 import pygame
-
-
-def do_grid(row_block_num, column_block_num):
-    num_of_dots = 0
-    for row in range(row_block_num):
-        row_progress = (row * width) + (margin * row) + margin
-        for column in range(column_block_num):
-            progress = (column * width) + (margin * column)
-            if grid[row][column] == 1:
-                pygame.draw.rect(screen, GREEN, [progress + margin, row_progress, width, height])
-            elif grid[row][column] == 3:
-                pygame.draw.rect(screen, YELLOW, [progress + margin, row_progress, width, height])
-                num_of_dots += 1
-            elif grid[row][column] == 0:
-                pygame.draw.rect(screen, WHITE, [progress + margin, row_progress, width, height])
-    return num_of_dots
+from GameClass import *
 
 
 def is_dot_next(current_row, current_column):
     if grid[current_row][current_column] == 3:
         return True
     return False
-
-
-def move_up(current_row, current_column, score):
-    if current_row != 0:
-        grid[current_row][current_column] = 0
-        current_row -= 1
-        if is_dot_next(current_row, current_column):
-            score += 1
-    grid[current_row][current_column] = 1
-    return current_row, current_column, score
-
-
-def move_down(current_row, current_column, score):
-    if current_row != row_block_num - 1:
-        grid[current_row][current_column] = 0
-        current_row += 1
-        if is_dot_next(current_row, current_column):
-            score += 1
-        grid[current_row][current_column] = 1
-    return current_row, current_column, score
-
-
-def move_left(current_row, current_column, score):
-    if current_column != 0:
-        grid[current_row][current_column] = 0
-        current_column -= 1
-    if is_dot_next(current_row, current_column):
-        score += 1
-    grid[current_row][current_column] = 1
-    return current_row, current_column, score
-
-
-def move_right(current_row, current_column, score):
-    if current_column != column_block_num - 1:
-        grid[current_row][current_column] = 0
-        current_column += 1
-    if is_dot_next(current_row, current_column):
-        score += 1
-    grid[current_row][current_column] = 1
-    return current_row, current_column, score
 
 
 pygame.init()
@@ -83,34 +28,17 @@ pygame.display.set_caption("New game")
 score = 0
 done = False
 clock = pygame.time.Clock()
-width = 20
-height = 20
-margin = 5
-grid = []
-for row in range(row_block_num):
-    grid.append([])
-    for column in range(column_block_num):
-        grid[row].append(3)
-current_row = 0
-current_column = 0
-grid[current_row][current_column] = 1
-num_of_dots = do_grid(row_block_num, column_block_num)
+current_row = 2
+current_column = 1
+player = Player(current_row, current_column)  # <--- declare the player, as an object, NOT A TUPLE
+grid = Greed(row_block_num, column_block_num, screen, player)  # <---- the game board, NOT AN ARRAY FFS GUY
+num_of_dots = grid.draw_grid()
 screen.blit(font.render(str(score), True, RED), [10, 10])
 pygame.display.flip()
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                current_row, current_column, score = move_up(current_row, current_column, score)
-            if event.key == pygame.K_DOWN:
-                current_row, current_column, score = move_down(current_row, current_column, score)
-            if event.key == pygame.K_LEFT:
-                current_row, current_column, score = move_left(current_row, current_column, score)
-            if event.key == pygame.K_RIGHT:
-                current_row, current_column, score = move_right(current_row, current_column, score)
-    num_of_dots = do_grid(row_block_num, column_block_num)
     if num_of_dots == 0:
         done = True
     screen.blit(font.render(str(score), True, RED), [10, 10])
